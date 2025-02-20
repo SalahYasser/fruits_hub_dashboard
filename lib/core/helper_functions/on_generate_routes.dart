efb7a7081e404b6d4384c5cba45e7839d47865a6
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-02-20 14:20:13 +0200

    create add product view

--- lib/core/helper_functions/on_generate_routes.dart
+++ lib/core/helper_functions/on_generate_routes.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub_dashboard/features/dashboard/views/dashboard_view.dart';
 
+import '../../features/add_product/presentation/views/add_product_view.dart';
+
 Route<dynamic> onGenerateRoutes(RouteSettings settings) {
   switch (settings.name) {
 
+
+    case AddProductView.routeName:
+      return MaterialPageRoute(builder: (context) => const AddProductView());
 
     case DashboardView.routeName:
       return MaterialPageRoute(builder: (context) => const DashboardView());
--- lib/features/dashboard/views/widgets/dashboard_view_body.dart
+++ lib/features/dashboard/views/widgets/dashboard_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub_dashboard/core/widgets/custom_button.dart';
+import 'package:fruits_hub_dashboard/features/add_product/presentation/views/add_product_view.dart';
 
 class DashboardViewBody extends StatelessWidget {
   const DashboardViewBody({super.key});
@@ -11,7 +12,12 @@ class DashboardViewBody extends StatelessWidget {
       children: [
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: CustomButton(text: 'Add Note', onPressed: () {}),
+          child: CustomButton(
+            text: 'Add Note',
+            onPressed: () {
+              Navigator.pushNamed(context, AddProductView.routeName);
+            },
+          ),
         ),
       ],
     );
