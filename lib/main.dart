9c88a17658df732cff19312a8fff20a98dd9da1e
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-02-20 12:04:19 +0200

    create dashboard

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub_dashboard/core/helper_functions/on_generate_routes.dart';
+
+void main() {
+  runApp(const FruitsHubDashboard());
+}
+
+class FruitsHubDashboard extends StatelessWidget {
+  const FruitsHubDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: onGenerateRoutes,
+    );
+  }
+}
