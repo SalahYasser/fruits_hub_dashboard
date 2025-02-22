27d527a56001fbbd14194f55e1feb37d4e5d8358
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-02-22 14:17:42 +0200

    get image url

--- lib/core/services/supabase_storage_service.dart
+++ lib/core/services/supabase_storage_service.dart
@@ -3,14 +3,12 @@ import 'package:path/path.dart' as b;
 import 'package:fruits_hub_dashboard/core/services/storage_service.dart';
 import 'package:fruits_hub_dashboard/core/utils/app_api_keys.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-
 import '../../constants.dart';
 
 class SupabaseStorageService implements StorageService {
   static late Supabase _supabase;
 
   static createBuckets(String bucketName) async {
-
     var buckets = await _supabase.client.storage.listBuckets();
 
     bool isBucketExists = false;
@@ -42,6 +40,10 @@ class SupabaseStorageService implements StorageService {
     var result = await _supabase.client.storage
         .from(kBucketName)
         .upload('$path/$fileName.$extension', file);
+
+    final String publicUrl = _supabase.client.storage
+        .from(kBucketName)
+        .getPublicUrl('$path/$fileName.$extension');
 
     return result;
   }
