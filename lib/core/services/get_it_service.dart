00d8e986d35f279c4268d24024d1d1fccbfeffcb
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-03-12 17:18:48 +0200

    convert to fire storage

--- lib/core/services/get_it_service.dart
+++ lib/core/services/get_it_service.dart
@@ -6,15 +6,16 @@ import 'package:fruits_hub_dashboard/core/services/supabase_storage_service.dart
 import 'package:get_it/get_it.dart';
 import '../repos/product_repo/products_repo.dart';
 import '../repos/product_repo/products_repo_impl.dart';
+import 'fire_storage.dart';
 import 'firestore_service.dart';
 
 final getIt = GetIt.instance;
 
 void setupGetIt() {
 
-  // getIt.registerSingleton<StorageService>(FireStorage());
+  getIt.registerSingleton<StorageService>(FireStorage());
 
-  getIt.registerSingleton<StorageService>(SupabaseStorageService());
+  // getIt.registerSingleton<StorageService>(SupabaseStorageService());
 
   getIt.registerSingleton<ImagesRepo>(
     ImagesRepoImpl(getIt.get<StorageService>()),
